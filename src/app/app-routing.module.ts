f9592703f0e99ddfb1f9ec6ce75d17d0cd8cae4f
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-09-24 17:26:00 -0400

    atualizacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { ListarRoupaComponent } from './listar-roupa/listar-roupa.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarSetorComponent } from './listar-setor/listar-setor.component';
 
 const routes: Routes = [
-  {path:'listarroupas', component: ListarRoupaComponent}
+  {path:'listarroupas', component: ListarRoupaComponent},
+  {path:'listarsetor', component: ListarSetorComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarRoupaComponent } from './listar-roupa/listar-roupa.component';
+import { ListarSetorComponent } from './listar-setor/listar-setor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarRoupaComponent
+    ListarRoupaComponent,
+    ListarSetorComponent
   ],
   imports: [
     BrowserModule,
