d6139a4ac6c57570b1ec8ac749240f6b565284a6
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-09-29 13:50:34 -0400

    Dropdownlist

--- src/app/incluir-funcionario/incluir-funcionario.component.ts
+++ src/app/incluir-funcionario/incluir-funcionario.component.ts
@@ -1,7 +1,11 @@
+import { FuncaoService } from './../services/funcao.service';
+import { Setor } from './../entities/setor';
+import { SetorService } from './../services/setor.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Funcionario } from '../entities/funcionario';
 import { FuncionarioService } from '../services/funcionario.service';
+import { funcao } from '../entities/funcao';
 
 @Component({
   selector: 'app-incluir-funcionario',
@@ -10,11 +14,22 @@ import { FuncionarioService } from '../services/funcionario.service';
 })
 export class IncluirFuncionarioComponent implements OnInit {
 
-funcionario: Funcionario={idfuncionario:0,nome:'',cpf:'',idSetor:0,email:'',salario:0,idFuncao:0,datadeContratacao: new Date()}
-
-  constructor(private funcionarioService: FuncionarioService) { }
+  funcionario: Funcionario={idfuncionario:0,nome:'',cpf:'',idSetor:0,email:'',salario:0,idFuncao:0,datadeContratacao: new Date()}
+  setores: Setor[] = []
+  funcoes: funcao[] = []
+  constructor(private funcionarioService: FuncionarioService, private setorService: SetorService, private funcaoService: FuncaoService) { }
 
   ngOnInit(): void {
+
+    this.setorService.listar().subscribe(
+      dados => this.setores = dados,
+      error=> alert('Erro ao recuperar setores')
+    )
+    this.funcaoService.listar().subscribe(
+      dados => this.funcoes = dados,
+      error => alert('Erro a recuperar funcoes')
+    )
+
   }
   
   incluir(frm: NgForm){
