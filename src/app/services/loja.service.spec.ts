03fddfdedb6666e9d409b9c8e882d106f88e7efa
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-10-06 13:51:56 -0400

    criação do teste serviceloja

--- src/app/services/loja.service.spec.ts
+++ src/app/services/loja.service.spec.ts
@@ -1,16 +1,37 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Loja } from '../entities/loja';
+import * as $ from 'jquery'
 
 import { LojaService } from './loja.service';
 
 describe('LojaService', () => {
   let service: LojaService;
+  let lojas: Loja[]=[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ 
+      imports:[HttpClientTestingModule],
+      providers: [LojaService] });
     service = TestBed.inject(LojaService);
+    
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Teste de Listar do componente com BackEnd', function(doneFn) {
+    $.ajax({
+        url: 'http://localhost:8080/loja/listar',
+        dataType: 'json',
+        success: function (data: Loja[], response: any) {
+            expect(data.length).toBeGreaterThanOrEqual(0)
+            doneFn();
+        },
+        error: function( data, response){
+          expect( function(){ true; } ).toThrow(new Error("Parsing is not possible"));
+        }
+    });
+  }); 
 });
