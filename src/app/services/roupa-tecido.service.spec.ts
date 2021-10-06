23a33036101fb2523992bfa188afb4e6e4826392
Marciel S. Soares <msoares@itelinfo.com>
2021-10-06 14:37:02 -0400

    fsf

--- src/app/services/roupa-tecido.service.spec.ts
+++ src/app/services/roupa-tecido.service.spec.ts
@@ -1,16 +1,37 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RoupaTecido } from '../entities/roupatecido';
+import * as $ from 'jquery'
 
 import { RoupaTecidoService } from './roupa-tecido.service';
 
 describe('RoupaTecidoService', () => {
   let service: RoupaTecidoService;
+  let RoupaTecido: RoupaTecido[]=[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ 
+      imports:[HttpClientTestingModule],
+      providers: [RoupaTecidoService] });
     service = TestBed.inject(RoupaTecidoService);
+    
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Teste de Listar do componente com BackEnd', function(doneFn) {
+    $.ajax({
+        url: 'http://localhost:8080/roupatecido/listar',
+        dataType: 'json',
+        success: function (data: RoupaTecido[], response: any) {
+            expect(data.length).toBeGreaterThanOrEqual(0)
+            doneFn();
+        },
+        error: function( data, response){
+          expect( function(){ true; } ).toThrow(new Error("Parsing is not possible"));
+        }
+    });
+  }); 
 });
