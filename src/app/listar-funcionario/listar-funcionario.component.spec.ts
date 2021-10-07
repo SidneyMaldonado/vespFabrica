91fe7d0239ef764737cea8eca88e37f82d263815
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-10-07 15:57:05 -0400

    Arrumanto teste de create

--- src/app/listar-funcionario/listar-funcionario.component.spec.ts
+++ src/app/listar-funcionario/listar-funcionario.component.spec.ts
@@ -1,31 +1,43 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Funcionario } from '../entities/funcionario';
 import { FuncionarioService } from '../services/funcionario.service';
-
 import { ListarFuncionarioComponent } from './listar-funcionario.component';
 
 describe('ListarFuncionarioComponent', () => {
   let component: ListarFuncionarioComponent;
   let fixture: ComponentFixture<ListarFuncionarioComponent>;
+  let h1: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListarFuncionarioComponent ],
-      imports:[HttpClientTestingModule],
+      imports:[HttpClientModule],
       providers:[FuncionarioService]
     })
     .compileComponents();
+    const fixture = TestBed.createComponent(ListarFuncionarioComponent);
+    component = fixture.componentInstance
+
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListarFuncionarioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('#Init - O componete deve ser criado', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get', ()=> expect(component.funcionarios.length).toBeGreaterThan(0))
+  it('#Dados - Deveria conter dados em funcionarios', (done)=> {
+    let funcionarios: Funcionario[] = component.listaMock();
+    let expected: number = funcionarios.length
+    let result: number = 0;
+    console.log("quantidade de funcionarios:", expected);
+    expect(expected).toBeGreaterThan(result);
+    done();
+  });
+
+  it('#Title - Deveria ter o titulo original em h1', async(() =>{
+    const fixture = TestBed.createComponent(ListarFuncionarioComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toContain("Lista de Funcionarios");
+
+  }));
 });
--- src/app/listar-funcionario/listar-funcionario.component.ts
+++ src/app/listar-funcionario/listar-funcionario.component.ts
@@ -9,6 +9,7 @@ import { FuncionarioService } from '../services/funcionario.service';
   styleUrls: ['./listar-funcionario.component.css']
 })
 export class ListarFuncionarioComponent implements OnInit {
+  title = "Lista de Funcionarios";
 
   funcionarios: Funcionario[] = [];
   constructor(private servicoFuncionario: FuncionarioService) { }
@@ -18,6 +19,34 @@ export class ListarFuncionarioComponent implements OnInit {
       dados => { this.funcionarios= dados, console.log(this.funcionarios) },
       error => { console.log("Erro ao recuperar roupas ", error)}
     )
+  }
+
+  listaMock(): Funcionario[] {
+    let registros: Funcionario[] = [
+      {
+        "idfuncionario": 32,
+        "nome": "Matheus Tomi",
+        "cpf": "65779758093",
+        "idSetor": 3,
+        "email": "matheus@gmail.com",
+        "salario": 1200.0,
+        "idFuncao": 1,
+        "datadeContratacao": new Date("2019-01-01")
+      },
+      {
+        "idfuncionario": 33,
+        "nome": "Matheus Tomi",
+        "cpf": "65779758093",
+        "idSetor": 3,
+        "email": "matheus@gmail.com",
+        "salario": 1200.0,
+        "idFuncao": 1,
+        "datadeContratacao": new Date("2019-01-01")
+      }
+    ]
+
+    return registros
+
   }
 
 }
